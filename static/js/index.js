7cf76af3f5a1cf8727b5ba6f59a01f55a0af3f48
kanogame <wasdqert1222@gmail.com>
2023-03-17 19:41:44 +0700

    frontend 2/3 done

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,11 @@
-class scrollBar {
-    function createScrollBar() {
-        
-    }
-}
+import WOW from '../node_modules/wow.js/src/WOW.js' 
+
+"use strict"
+const wow = new WOW({
+  boxClass: 'wow',
+  animateClass: 'animated',
+  offset: 0,
+  live: true
+});
+wow.init();
+console.log("wow loaded");
