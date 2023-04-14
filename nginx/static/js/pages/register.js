39ecbe612dfc6ba34aeb517c022d42f09b6eb8cd
kanogame <wasdqert1222@gmail.com>
2023-04-15 00:11:06 +0700

    fuck

--- nginx/static/js/pages/register.js
+++ nginx/static/js/pages/register.js
@@ -2,7 +2,7 @@
 import PostConnection from "../utils/post.js";
 import CookieManager from "../utils/cookieManager.js";
 
-const url = "http://192.168.1.7:10234/reg";
+const url = "http://proxy-api:10234/reg";
 const regForm = document.getElementById("regForm");
 const cookie = new CookieManager();
 
